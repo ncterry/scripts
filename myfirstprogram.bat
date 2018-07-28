0528e74180fdba1e395997b6fddee375ed76a89c
Nate Terry <nate@ncterry.com>
2018-07-28 15:08:37 -0600

    First commit to remote repo

---
+++ myfirstprogram.bat
@@ -0,0 +1,13 @@
+: Every batch file. Stops file pats from being repeated. More professional.
+@echo off
+: Create a top bar title for your batch file.
+title My First Batch File
+: Make it say something
+echo Hello
+: Pause program, press any key to continue
+pause
+: cls will clear the screen prior to it d
+cls
+echo This is the first line
+echo This is the second line
+pause
